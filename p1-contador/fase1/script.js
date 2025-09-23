b270084a3c96600c11be7c443684308132cb000e
Jose Alejandro Rodriguez Gil <josealejrg@gmail.com>
2025-09-23 11:47:42 +0200

    Mejoras de aspecto y técnicas

--- p1-contador/fase1/script.js
+++ p1-contador/fase1/script.js
@@ -4,20 +4,33 @@ const btnMas = document.getElementById("btn-mas");
 const btnMenos = document.getElementById("btn-menos");
 
 function actualizarContador() {
-  spanContador.textContent = contador;
+  spanContador.textContent = parseFloat(contador).toFixed(1);
 
   // Efecto visual para marcar el cambio
   spanContador.classList.add("changed");
-  setTimeout(() => spanContador.classList.remove("changed"), 200);
+  setTimeout(() => spanContador.classList.remove("changed"), 350);
+  efectoColorDelContador();
+}
+function efectoColorDelContador() {
+  if(contador > 8) {
+    spanContador.style.color = "green";
+  } else if(contador > 5) {
+    spanContador.style.color = "orange";
+  } else if(contador > 3) {
+    spanContador.style.color = "red";
+  }
+
 }
 
 btnMas.addEventListener("click", () => {
-  contador++;
+  if(contador > 9.9) return;
+  contador += 0.1;
   actualizarContador();
 });
 
 btnMenos.addEventListener("click", () => {
-  contador--;
+  if(contador < 0.1) return;
+  contador -= 0.1;
   actualizarContador();
 });
 
