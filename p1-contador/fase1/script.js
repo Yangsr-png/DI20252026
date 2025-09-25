376554383d50de910409d38e17dddd06d6efa653
Alejandro <jandroaguilon17@gmail.com>
2025-09-25 10:11:40 +0200

    Mi primera práctica

--- p1-contador/fase1/script.js
+++ p1-contador/fase1/script.js
@@ -1,4 +1,4 @@
-let contador = 10;
+let contador = 20;
 const spanContador = document.getElementById("contador");
 const btnMas = document.getElementById("btn-mas");
 const btnMenos = document.getElementById("btn-menos");
@@ -12,25 +12,25 @@ function actualizarContador() {
   efectoColorDelContador();
 }
 function efectoColorDelContador() {
-  if(contador > 8) {
-    spanContador.style.color = "green";
-  } else if(contador > 5) {
-    spanContador.style.color = "orange";
-  } else if(contador > 3) {
+  if(contador >1 && contador <=10) {
     spanContador.style.color = "red";
+  } else if(contador > 10 && contador <= 15) {
+    spanContador.style.color = "orange";
+  } else if(contador > 15 && contador <= 20) {
+    spanContador.style.color = "green";
   }
 
 }
 
 btnMas.addEventListener("click", () => {
-  if(contador > 9.9) return;
-  contador += 0.1;
+  if(contador > 19.5) return;
+  contador += 0.5;
   actualizarContador();
 });
 
 btnMenos.addEventListener("click", () => {
-  if(contador < 0.1) return;
-  contador -= 0.1;
+  if(contador < 0.5) return;
+  contador -= 0.5;
   actualizarContador();
 });
 
