19c073ee163ca5a55bbe88fc0718c0d019133496
jorge1sbr <jorge.p1998@hotmail.com>
2025-09-25 13:22:36 +0200

    Primeros cammbios

--- p1-contador/fase1/script.js
+++ p1-contador/fase1/script.js
@@ -1,10 +1,10 @@
-let contador = 10;
+/*let contador = 10;
 const spanContador = document.getElementById("contador");
 const btnMas = document.getElementById("btn-mas");
 const btnMenos = document.getElementById("btn-menos");
 
 function actualizarContador() {
-  spanContador.textContent = contador;
+  spanContador.textContent = parseFloat(contador).toFixed(1);
 
   // Efecto visual para marcar el cambio
   spanContador.classList.add("changed");
@@ -12,14 +12,47 @@ function actualizarContador() {
 }
 
 btnMas.addEventListener("click", () => {
-  contador++;
+  contador = contador +0.1;
+  if (contador > 10) {
+  contador = 10;
+}
+
   actualizarContador();
 });
 
 btnMenos.addEventListener("click", () => {
-  contador--;
+  contador = contador -0.1;
+  if (contador < 0) {
+  contador = 0;
+}
+
   actualizarContador();
 });
 
 // Inicialización
 actualizarContador();
+
+document.addEventListener*/
+
+//LISTA DE TODAS LAS TARJETAS GUARDADA EN tarjetas
+const tarjetas = document.querySelectorAll(".contador")
+
+tarjetas.forEach((tarjeta) => {
+  const spanValor = tarjeta.querySelector(".valor");
+  const btnMas = tarjeta.querySelector(".btn-mas");
+  const btnMenos = tarjeta.querySelector(".btn-menos");
+
+  btnMas.addEventListener("click", ()=>{
+  let valor = parseFloat(spanValor.textContent);
+  valor = valor +0.1;
+  spanValor.textContent = valor.toFixed(1);
+  });
+
+  btnMenos.addEventListener("click", ()=>{
+    let valor = parseFloat(spanValor.textContent);
+    valor = valor -0.1;
+    spanValor.text
+  })
+
+});
+
