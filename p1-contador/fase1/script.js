a8e4d2bcbfba77b9e8256dbd7f8b645f9ec813d5
Freddy <freddycatala31@gmail.com>
2025-09-25 13:26:20 +0200

    seleccionar decimales/boton0/limite/teclado

--- p1-contador/fase1/script.js
+++ p1-contador/fase1/script.js
@@ -1,16 +1,76 @@
 let contador = 10;
+let contdec=0.1;
+
 const spanContador = document.getElementById("contador");
+const spanDecimal= document.getElementById("dec-valor");
 const btnMas = document.getElementById("btn-mas");
 const btnMenos = document.getElementById("btn-menos");
+const btn01Mas = document.getElementById("btn-01mas");
+const btn01Menos = document.getElementById("btn-01menos");
+const decMas = document.getElementById("dec-mas");
+const decMenos = document.getElementById("dec-menos");
+const btnzero = document.getElementById("btn-zero");
+const contenedor = document.getElementById("contador-container");
+const decimales = document.getElementById("decimales");
 
-function actualizarContador() {
-  spanContador.textContent = contador;
+window.addEventListener("DOMContentLoaded", () => {
+  decimales.selectedIndex = 0;
+});
+
+decimales.addEventListener("mouseleave", () => {
+  decimales.blur();
+  
+});
+decMas.addEventListener("click", () => {
+  contdec=contdec+0.1;
+  contdec=Number(contdec.toFixed(1));
+  actualizarContador();
+});
+decMenos.addEventListener("click", () => {
+  contdec=contdec-0.1;
+  contdec=Number(contdec.toFixed(1));
+  actualizarContador();
+});
+
+
+function actualizarContador() {  
+  limitarContador();
+  spanContador.textContent = contador.toFixed(1);
+  spanDecimal.textContent = contdec;
+  btn01Mas.textContent = `+${contdec}`;
+   btn01Menos.textContent = `-${contdec}`;
 
   // Efecto visual para marcar el cambio
   spanContador.classList.add("changed");
   setTimeout(() => spanContador.classList.remove("changed"), 200);
 }
 
+document.addEventListener("keydown", (event) => {
+  if (contenedor.mouseover || contenedor.matches(':hover')) {
+    switch (event.key) {
+      case "ArrowUp":
+        contador++;
+        actualizarContador();
+        break;
+      case "ArrowDown":
+        contador--;
+        actualizarContador();
+        break;
+      case "ArrowRight":
+        contador=contador + contdec;
+        actualizarContador();
+        break;
+      case "ArrowLeft":
+        contador=contador - contdec;
+        actualizarContador();
+        break;
+      case "0":
+        contador = 0;
+        actualizarContador();
+        break;
+    }
+  }
+});
 btnMas.addEventListener("click", () => {
   contador++;
   actualizarContador();
@@ -20,6 +80,53 @@ btnMenos.addEventListener("click", () => {
   contador--;
   actualizarContador();
 });
+btn01Mas.addEventListener("click", () => {
+  contador=contador + contdec;
+  actualizarContador();
+});
+
+btn01Menos.addEventListener("click", () => {
+  contador=contador - contdec;
+  actualizarContador();
+});
+btnzero.addEventListener("click", () => {
+  contador= 0;
+  actualizarContador();
+});
+
+
+
+
+function limitarContador() {
+  btnMas.disabled = false;
+  btnMenos.disabled = false;
+  btn01Mas.disabled = false;
+  btn01Menos.disabled = false;
+  decMas.disabled = false;
+  decMenos.disabled = false;
+  if (contador <=0){
+    contador=0;
+    btnMas.disabled = false;
+    btn01Mas.disabled = false;
+    btnMenos.disabled = true;
+    btn01Menos.disabled = true;
+  }else if (contador >=10){
+    contador=10;
+    btnMas.disabled = true;
+    btn01Mas.disabled = true;
+    btnMenos.disabled = false;
+    btn01Menos.disabled = false;
+  }
+  if (contdec<=0.1){
+    contdec=0.1;
+    decMas.disabled = false;
+    decMenos.disabled = true;
+  }else if (contdec>=1.0){
+    contdec=0.9;
+    decMas.disabled = true;
+    decMenos.disabled = false;
+  }
+}
 
 // Inicialización
 actualizarContador();
