4b395d53c1afd122f14a7851496d36641f00a3c6
Gustavo <gusparsal@protonmail.com>
2025-09-25 13:25:41 +0200

    Contador limitado y suma o resta +-0.1

--- p1-contador/fase1/script.js
+++ p1-contador/fase1/script.js
@@ -6,18 +6,34 @@ const btnMenos = document.getElementById("btn-menos");
 function actualizarContador() {
   spanContador.textContent = contador;
 
+  // habilitar/deshabilitar botones según el valor del contador
+  if (contador >= 10) {
+    btnMas.disabled = true;
+  } else if (contador < 10) {
+    btnMas.disabled = false;
+  }
+  if (contador <= 0) {
+    btnMenos.disabled = true;
+  } else if (contador > 0) {
+    btnMenos.disabled = false;
+  }
+
   // Efecto visual para marcar el cambio
   spanContador.classList.add("changed");
   setTimeout(() => spanContador.classList.remove("changed"), 200);
 }
 
 btnMas.addEventListener("click", () => {
-  contador++;
+  contador += 0.1;
+  contador = Math.round(contador * 10) / 10;
+
   actualizarContador();
 });
 
 btnMenos.addEventListener("click", () => {
-  contador--;
+  contador -= 0.1;
+  contador = Math.round(contador * 10) / 10;
+  
   actualizarContador();
 });
 
